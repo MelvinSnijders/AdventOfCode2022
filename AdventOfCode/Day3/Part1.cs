138118aab3b4b9ff615c4432c4a49bdfda294cd7
Melvin Snijders <mail@melvinsnijders.nl>
2022-12-04 15:48:59 +0100

    Improved day 3

--- AdventOfCode/Day3/Part1.cs
+++ AdventOfCode/Day3/Part1.cs
@@ -13,25 +13,12 @@ public class Part1
     public int Solve()
     {
 
-        int prioritySum = 0;
-        foreach (string rucksack in _rucksacks)
-        {
-            string compartment1 = rucksack.Substring(0, rucksack.Length / 2);
-            string compartment2 = rucksack.Substring(rucksack.Length / 2);
-
-            char duplicate = compartment1.ToList().Find(character => compartment2.Contains(character));
-
-            int code = 0;
-            if(char.IsUpper(duplicate)) code = duplicate - 38; // Uppercase = 27-52
-            if (char.IsLower(duplicate)) code = duplicate - 96; // Lowercase = 1-26
-
-            Console.WriteLine($"{duplicate} {code}");
-            
-            prioritySum += code;
-
-        }
-        
-        return prioritySum;
+        return _rucksacks.Select(rucksack =>
+                rucksack.Substring(0, rucksack.Length / 2)
+                    .Intersect(rucksack.Substring(rucksack.Length / 2))
+                    .First())
+            .Select(ch => Char.IsUpper(ch) ? ch - 38 : ch - 96)
+            .Sum();
 
     }
 }
--- AdventOfCode/Day3/Part2.cs
+++ AdventOfCode/Day3/Part2.cs
@@ -13,33 +13,13 @@ public class Part2
     public int Solve()
     {
 
-        int prioritySum = 0;
-        
-        // Group into chunks of 3
-        List<string[]> groups = new List<string[]>();
-        for (int i = 0; i < _rucksacks.Length; i += 3)
-        {
-            string[] buffer = new string[3];
-            Array.Copy(_rucksacks, i, buffer, 0, 3);
-            groups.Add(buffer);
-        }
-        
-        // Process
-        foreach (string[] group in groups)
-        {
-            string rucksack1 = group[0];
-            string rucksack2 = group[1];
-            string rucksack3 = group[2];
+        return _rucksacks
+            .Chunk(3)
+            .Select(group => 
+                group[0].Intersect(group[1]).Intersect(group[2])
+                    .First())
+            .Select(ch => Char.IsUpper(ch) ? ch - 38 : ch - 96)
+            .Sum();
 
-            char intersect = rucksack1.Intersect(rucksack2).Intersect(rucksack3).First();
-            Console.WriteLine(intersect);
-            int code = 0;
-            if(char.IsUpper(intersect)) code = intersect - 38; // Uppercase = 27-52
-            if (char.IsLower(intersect)) code = intersect - 96; // Lowercase = 1-26
-            prioritySum += code;
-        }
-
-        return prioritySum;
-        
     }
 }
