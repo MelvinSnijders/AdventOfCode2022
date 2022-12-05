3df7dba5986fc9087ae377c670eab064b32baeec
Melvin Snijders (student) <s1154771@student.windesheim.nl>
2022-12-05 10:19:48 +0100

    Day 5

---
+++ AdventOfCode/Day5/Part1.cs
@@ -0,0 +1,55 @@
+using System.Text;
+
+namespace AdventOfCode.Day5;
+
+public class Part1
+{
+    private readonly string[] _moves;
+
+    public Part1()
+    {
+        string input = File.ReadAllText("Day5/input.txt");
+        string[] parts = input.Split("\n\n");
+        _moves = parts[1].Split("\n");
+    }
+
+    public string Solve()
+    {
+        // Setup the stacks, too lazy to read from input
+        Stack<char>[] stacks = new Stack<char>[9];
+        stacks[0] = new Stack<char>(new[] { 'R', 'S', 'L', 'F', 'Q' });
+        stacks[1] = new Stack<char>(new[] { 'N', 'Z', 'Q', 'G', 'P', 'T' });
+        stacks[2] = new Stack<char>(new[] { 'S', 'M', 'Q', 'B' });
+        stacks[3] = new Stack<char>(new[] { 'T', 'G', 'Z', 'J', 'H', 'C', 'B', 'Q' });
+        stacks[4] = new Stack<char>(new[] { 'P', 'H', 'M', 'B', 'N', 'F', 'S' });
+        stacks[5] = new Stack<char>(new[] { 'P', 'C', 'Q', 'N', 'S', 'L', 'V', 'G' });
+        stacks[6] = new Stack<char>(new[] { 'W', 'C', 'F' });
+        stacks[7] = new Stack<char>(new[] { 'Q', 'H', 'G', 'Z', 'W', 'V', 'P', 'M' });
+        stacks[8] = new Stack<char>(new[] { 'G', 'Z', 'D', 'L', 'C', 'N', 'R' });
+
+        // Loop through the moves     
+        foreach (string move in _moves)
+        {
+            string[] delimiter = { "move ", " from ", " to " };
+            int[] moveNumbers = move.Split(delimiter, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine($"Move {moveNumbers[0]} from {moveNumbers[1]} to {moveNumbers[2]}");
+
+            for (int i = 0; i < moveNumbers[0]; i++)
+            {
+                char toMove = stacks[moveNumbers[1] - 1].Pop();
+                stacks[moveNumbers[2] - 1].Push(toMove);
+            }
+        }
+
+        // Get top of all stacks
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (Stack<char> stack in stacks)
+        {
+            stringBuilder.Append(stack.Peek());
+        }
+
+        return stringBuilder.ToString();
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -6,6 +6,8 @@ using Day3Part1 = AdventOfCode.Day3.Part1;
 using Day3Part2 = AdventOfCode.Day3.Part2;
 using Day4Part1 = AdventOfCode.Day4.Part1;
 using Day4Part2 = AdventOfCode.Day4.Part2;
+using Day5Part1 = AdventOfCode.Day5.Part1;
+using Day5Part2 = AdventOfCode.Day5.Part2;
 
 Console.WriteLine($"Day 1, Part 1: {new Day1Part1().Solve()}");
 Console.WriteLine($"Day 1, Part 2: {new Day1Part2().Solve()}");
@@ -19,3 +21,5 @@ Console.WriteLine($"Day 3, Part 2: {new Day3Part2().Solve()}");
 Console.WriteLine($"Day 4, Part 1: {new Day4Part1().Solve()}");
 Console.WriteLine($"Day 4, Part 2: {new Day4Part2().Solve()}");
 
+Console.WriteLine($"Day 5, Part 1: {new Day5Part1().Solve()}");
+Console.WriteLine($"Day 5, Part 2: {new Day5Part2().Solve()}");
