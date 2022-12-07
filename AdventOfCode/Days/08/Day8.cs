64c14ba77c05792fa14c204c0832b30d12093d14
Melvin Snijders <mail@melvinsnijders.nl>
2022-12-07 22:25:03 +0100

    Day 8 preparation

---
+++ AdventOfCode/Days/08/Day8.cs
@@ -0,0 +1,22 @@
+using AoCHelper;
+
+namespace AdventOfCode.Days._08;
+
+public sealed class Day8 : BaseDay
+{
+
+    public Day8()
+    {
+        string input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+}
