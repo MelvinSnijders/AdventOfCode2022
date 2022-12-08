eb0d91140d2fc696193035a96f52db4281e01abc
Melvin Snijders (student) <s1154771@student.windesheim.nl>
2022-12-08 10:05:38 +0100

    Day 9 preparation

---
+++ AdventOfCode/Days/09/Day9.cs
@@ -0,0 +1,22 @@
+using AoCHelper;
+
+namespace AdventOfCode.Days._09;
+
+public sealed class Day9 : BaseDay
+{
+    
+    public Day9()
+    {
+        string input = File.ReadAllText(InputFilePath);
+    }
+    
+    public override ValueTask<string> Solve_1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+}
